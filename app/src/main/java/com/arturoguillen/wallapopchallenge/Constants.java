0178c91a7ca9b170c25f71bf0d615b84213545ff
Arturo Guillen <sammyjankis87@gmail.com>
2017-03-08 21:49:26 +0100

    Added constants classes

---
+++ app/src/main/java/com/arturoguillen/wallapopchallenge/Constants.java
@@ -0,0 +1,12 @@
+package com.arturoguillen.wallapopchallenge;
+
+/**
+ * Created by artu on 3/8/17.
+ */
+
+public class Constants {
+    public static final String BASE_URL = "http://gateway.marvel.com/v1/public/";
+    public static final int CAPTAIN_AMERICA_ID = 1009220;
+
+
+}
