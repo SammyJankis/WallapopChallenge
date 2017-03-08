8335e1205179e59936df9ce1d3bd97811ae2c61c
Arturo Guillen <sammyjankis87@gmail.com>
2017-03-08 08:12:39 +0100

    Added base structure for FeedActivity

---
+++ app/src/main/java/com/arturoguillen/wallapopchallenge/view/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.arturoguillen.wallapopchallenge.view;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by artu on 3/8/17.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/arturoguillen/wallapopchallenge/view/FeedActivity.java
@@ -0,0 +1,19 @@
+package com.arturoguillen.wallapopchallenge.view;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.arturoguillen.wallapopchallenge.R;
+
+/**
+ * Created by artu on 3/8/17.
+ */
+
+public class FeedActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+    }
+}
