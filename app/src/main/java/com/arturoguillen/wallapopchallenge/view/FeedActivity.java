e8afdc99e400910e1c2c109e9d74fcf74677da32
Arturo Guillen <sammyjankis87@gmail.com>
2017-03-08 08:35:18 +0100

    Added ButterKnife to grade and project struck

--- app/src/main/java/com/arturoguillen/wallapopchallenge/view/FeedActivity.java
+++ app/src/main/java/com/arturoguillen/wallapopchallenge/view/FeedActivity.java
@@ -2,18 +2,25 @@ package com.arturoguillen.wallapopchallenge.view;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
 
 import com.arturoguillen.wallapopchallenge.R;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by artu on 3/8/17.
  */
 
 public class FeedActivity extends BaseActivity {
+    @BindView(R.id.recyclerview_feed)
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+
+        ButterKnife.bind(this);
     }
 }
