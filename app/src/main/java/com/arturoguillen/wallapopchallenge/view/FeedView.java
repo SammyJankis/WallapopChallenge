319258c8883a9208564c1b727359a7f989fc4d7e
Arturo Guillen <sammyjankis87@gmail.com>
2017-03-09 20:23:43 +0100

    Added new methods to FeedView

--- app/src/main/java/com/arturoguillen/wallapopchallenge/view/FeedView.java
+++ app/src/main/java/com/arturoguillen/wallapopchallenge/view/FeedView.java
@@ -1,5 +1,11 @@
 package com.arturoguillen.wallapopchallenge.view;
 
+import android.support.annotation.StringRes;
+
+import com.arturoguillen.wallapopchallenge.entity.Comic;
+
+import java.util.ArrayList;
+
 /**
  * Created by artu on 3/8/17.
  */
@@ -9,4 +15,8 @@ public interface FeedView {
     void showProgressFooter();
 
     void hideProgressFooter();
+
+    void showMessage(@StringRes int stringId);
+
+    void showMoreData(ArrayList<Comic> comics);
 }
