cd3391ddbe6eb64eda7762fc1f2dab7c73c8ccab
Arturo Guillen <arturo.guillen@privalia.com>
2017-03-14 18:09:35 +0100

    Added MockClasses

---
+++ app/src/test/java/com/arturoguillen/wallapopchallenge/fabric/MockMarvelApi.java
@@ -0,0 +1,33 @@
+package com.arturoguillen.wallapopchallenge.fabric;
+
+import com.arturoguillen.wallapopchallenge.di.api.MarvelApi;
+
+import io.reactivex.Observable;
+
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by arturo.guillen on 14/03/2017.
+ */
+
+public class MockMarvelApi {
+
+    public static MarvelApi getMarvelApiMock() {
+        MarvelApi marvelApi = mock(MarvelApi.class);
+
+        when(
+                marvelApi.getComicsByCharacter(
+                        anyInt(),
+                        anyString(),
+                        anyString(),
+                        anyString(),
+                        anyString()
+                )
+        ).thenReturn(Observable.just(MockMarvelModel.getFakeComicDataWrapper()));
+
+        return marvelApi;
+    }
+}
